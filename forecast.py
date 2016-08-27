85e0db9ff9bf3cb3be31d80c35574b11f4f16702
Jerry Eshbaugh <j2m2e2@gmail.com>
2016-08-27 13:44:19 -0600

    Use JSON for location data

--- forecast.py
+++ forecast.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python
 
 import urllib
+import json
 
-def get_location(ip):
+def get_location(ip, service = 'http://ip-api.com/json/' ):
 
-  location = urllib.urlopen('http://ip-api.com/php/' + ip).read()
-  print(location)
-
+  print('------------')
+  location = urllib.urlopen(service + ip).read()
+  location_json = json.loads(location)
+  print(location_json['city'])
+  print(location_json['region'])
+  print(location_json['countryCode'])
 
 get_location('8.8.8.8')
 
